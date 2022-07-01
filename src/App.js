787c51477242b73725140287f0b6a10826b989e8
wmaKRET <wkret91.2@gmail.com>
2022-07-01 09:10:53 +0200

    added state to handle textarea

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from "react"
+import React, {useState} from "react"
 
 function App() {
+  const [text, setText] = useState('')
+
+  function handleChange(event){
+    const {value} = event.target
+    setText(value)
+  }
+
   return (
     <main>
       <h1 className="text">How fast can you type?</h1>
       {/* <h3>High score: 0</h3> */}
       <textarea 
+        name={text}
         placeholder="Type here..."
         disabled={false}
+        onChange={handleChange}
       />
       <h3 className="text">Time remaining: 0</h3>
       <button 
