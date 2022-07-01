398625ce6f9079780cdfefa6da28a9ae27f0b9e4
wmaKRET <wkret91.2@gmail.com>
2022-07-01 09:16:27 +0200

    changed placeholder color

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 
 function App() {
+  const TIMER_VALUE = 10
+
   const [text, setText] = useState('')
+  const [timeRemaining, setTimeRemaining] = useState(TIMER_VALUE)
+  //const [isTimeRunning, setIsTimeRunning] = useState(false)
 
   function handleChange(event){
     const {value} = event.target
     setText(value)
   }
+
+  // useEffect(() => {
+    
+  // }
+  // ,[timeRemaining])
 
   return (
     <main>
@@ -18,11 +27,12 @@ function App() {
         disabled={false}
         onChange={handleChange}
       />
-      <h3 className="text">Time remaining: 0</h3>
+      <h3 className="text">Time remaining: {timeRemaining}</h3>
       <button 
         className="text btn"
         disabled={false}
-      >START</button>
+      >START
+      </button>
       <h2 className="text">Word count: 0</h2>
     </main>
   )
