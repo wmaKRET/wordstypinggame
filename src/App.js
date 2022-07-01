b759974272c2d7d3843601b9ec01f193e94aa44e
wmaKRET <wkret91.2@gmail.com>
2022-07-01 09:05:09 +0200

    added app style

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import React from "react"
 function App() {
   return (
     <main>
-      <h1>How fast can you type?</h1>
+      <h1 className="text">How fast can you type?</h1>
       {/* <h3>High score: 0</h3> */}
-      <textarea />
-      <h3>Time remaining: 0</h3>
-      <button>START</button>
-      <h2>Word count: 0</h2>
+      <textarea 
+        placeholder="Type here..."
+        disabled={false}
+      />
+      <h3 className="text">Time remaining: 0</h3>
+      <button 
+        className="text btn"
+        disabled={false}
+      >START</button>
+      <h2 className="text">Word count: 0</h2>
     </main>
   )
 }
